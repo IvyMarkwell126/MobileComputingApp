e630b860b7f1a4824e1cb029db8e3467bfde320e
abelm1248 <abelm@hinq.cs.utexas.edu>
2017-03-22 18:11:55 -0500

    Login and registration pages
    Created login and registration pages

---
+++ loginRegister/Assets.xcassets/loginPageViewController.swift
@@ -0,0 +1,94 @@
+//
+//  loginPageViewController.swift
+//  loginRegister
+//
+//  Created by Abel Morales on 3/22/17.
+//  Copyright © 2017 Abel Morales. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class loginPageViewController: UIViewController {
+
+    var user: NSManagedObject?
+    var users = [NSManagedObject]()
+    var loginAlert:UIAlertController? = nil
+    
+    @IBOutlet var usernameField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func loadData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"User")
+        var fetchedResults:[NSManagedObject]? = nil
+        
+        do {
+            try fetchedResults = managedContext.fetch(fetchRequest) as? [NSManagedObject]
+        } catch {
+            // what to do if an error occurs?
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+        
+        if let results = fetchedResults {
+            users = results
+        } else {
+            print("Could not fetch")
+        }
+    }
+    
+    
+    
+    @IBAction func loginBtn(_ sender: Any) {
+        self.loadData()
+        let userName = usernameField.text
+        let passWord = passwordField.text
+        var loggedin = false
+        for elt in users {
+            var testName = (elt.value(forKey: "username") as? String)!
+            var testPass = (elt.value(forKey: "password") as? String)!
+            print ("\(testName) \(testPass)")
+            if(userName == "" || passWord == ""){
+                self.loginAlert = UIAlertController(title: "Error", message: "You cannot leave either field blank", preferredStyle: UIAlertControllerStyle.alert)
+                let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in
+                    //print("Ok Button Pressed 1");
+                }
+                self.loginAlert!.addAction(OKAction)
+                self.present(self.loginAlert!, animated: true, completion:nil)
+            }
+            else if(userName == testName && passWord == testPass){
+                self.loginAlert = UIAlertController(title: "Success", message: "You have successfully logged in", preferredStyle: UIAlertControllerStyle.alert)
+                let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in
+                    //print("Ok Button Pressed 1");
+                }
+                self.loginAlert!.addAction(OKAction)
+                self.present(self.loginAlert!, animated: true, completion:nil)
+                loggedin = true
+                break
+            }
+        }
+        if(!loggedin){
+            self.loginAlert = UIAlertController(title: "Error", message: "Username or password is incorrect", preferredStyle: UIAlertControllerStyle.alert)
+            let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in
+                //print("Ok Button Pressed 1");
+            }
+            self.loginAlert!.addAction(OKAction)
+            self.present(self.loginAlert!, animated: true, completion:nil)
+        }
+    }
+
+}
---
+++ loginRegister/registerPageViewController.swift
@@ -0,0 +1,126 @@
+//
+//  registerPageViewController.swift
+//  loginRegister
+//
+//  Created by Abel Morales on 3/22/17.
+//  Copyright © 2017 Abel Morales. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class registerPageViewController: UIViewController {
+
+    var user: NSManagedObject?
+    var users = [NSManagedObject]()
+    
+    @IBOutlet var usrField: UITextField!
+    @IBOutlet var passField: UITextField!
+    @IBOutlet var repassField: UITextField!
+    @IBOutlet var messageLbl: UILabel!
+    
+    var regisAlert:UIAlertController? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func loadData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"User")
+        var fetchedResults:[NSManagedObject]? = nil
+        
+        do {
+            try fetchedResults = managedContext.fetch(fetchRequest) as? [NSManagedObject]
+        } catch {
+            // what to do if an error occurs?
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+        
+        if let results = fetchedResults {
+            users = results
+        } else {
+            print("Could not fetch")
+        }
+    }
+    
+    @IBAction func saveBtn(_ sender: Any) {
+        self.loadData()
+        let userName = usrField.text!
+        let passWord = passField.text!
+        let rePassWord = repassField.text!
+        
+        for elt in users{
+            let testName = (elt.value(forKey: "username") as? String)!
+            if(testName == userName){
+                self.regisAlert = UIAlertController(title: "Error", message: "Username already taken", preferredStyle: UIAlertControllerStyle.alert)
+                let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in
+                    //print("Ok Button Pressed 1");
+                }
+                self.regisAlert!.addAction(OKAction)
+                self.present(self.regisAlert!, animated: true, completion:nil)
+            }
+        }
+        
+        if (usrField.text == "" || passField.text == "" || repassField.text == "") {
+            self.regisAlert = UIAlertController(title: "Error", message: "You must enter a value for all fields", preferredStyle: UIAlertControllerStyle.alert)
+            let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in
+                //print("Ok Button Pressed 1");
+            }
+            self.regisAlert!.addAction(OKAction)
+            self.present(self.regisAlert!, animated: true, completion:nil)
+        }
+            
+        else if (passWord != rePassWord){
+            self.regisAlert = UIAlertController(title: "Error", message: "Passwords do not match", preferredStyle: UIAlertControllerStyle.alert)
+            let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in
+                //print("Ok Button Pressed 1");
+            }
+            self.regisAlert!.addAction(OKAction)
+            self.present(self.regisAlert!, animated: true, completion:nil)
+        }
+        else{
+            
+            registerUser(passedLogin: userName, passedPassword: passWord, passedRePass: rePassWord)
+            
+            self.regisAlert = UIAlertController(title: "Success", message: "New User Registered", preferredStyle: UIAlertControllerStyle.alert)
+            let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in
+                //print("Ok Button Pressed 1");
+            }
+            self.regisAlert!.addAction(OKAction)
+            self.present(self.regisAlert!, animated: true, completion:nil)
+            
+        }
+    }
+    
+    func registerUser(passedLogin:String, passedPassword:String, passedRePass:String){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity =  NSEntityDescription.entity(forEntityName: "User", in: managedContext)
+        let candidate = NSManagedObject(entity: entity!, insertInto: managedContext)
+        
+        candidate.setValue(passedLogin, forKey: "username")
+        candidate.setValue(passedPassword, forKey: "password")
+        
+        do {
+            try managedContext.save()
+        } catch {
+            // what to do if an error occurs?
+            let nserror = error as NSError
+            print("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+    }
+
+}
