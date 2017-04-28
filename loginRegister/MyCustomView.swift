c5c03d82a580bc9cd05556da61cf4070a556af3b
abelm1248 <abelm@hinq.cs.utexas.edu>
2017-04-28 14:06:25 -0500

    prepared color changing function

---
+++ loginRegister/MyCustomView.swift
@@ -0,0 +1,21 @@
+//
+//  MyCustomView.swift
+//  loginRegister
+//
+//  Created by Abel Morales on 4/28/17.
+//  Copyright © 2017 Abel Morales. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
